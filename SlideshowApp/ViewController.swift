30cfcb829a3fa4ce3a1cc57f34a77a2ac6b4a82e
kei.46 <xxgsuqigtzg83akp3dms@docomo.ne.jp>
2020-10-18 19:54:24 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 鶴岡慧 on 2020/10/18.
+//  Copyright © 2020 kei.tsuruoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
